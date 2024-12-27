f2eb41d4175530bb41495fd54c2aedb4c3e4675b
Ashish_83 <117579752+Ashish8329@users.noreply.github.com>
2024-12-27 19:16:15 +0530

    Add files via upload

---
+++ Land-Registration-System-Using-Blockchain/lib/constant/constants.dart
@@ -0,0 +1,12 @@
+String mapBoxApiKey =
+    'pk.eyJ1Ijoic2F1cmFiaG13IiwiYSI6ImNreTRiYzNidjBhMTkydnB2dmpoeGt4ZmgifQ.2QZ4CsNiygDTAhkqASpbPg';
+
+String nftStorageApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGNBRjJiZjNDMjNkYTQ3OWMyRTZFNDUwODE4REZDMmVCMjlCNzkyZmMiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY5MzEzNTg1MzY4OCwibmFtZSI6IkxhbmRSZWdpc3RyYXRpb24ifQ.GzsTBmhroKkiHY_6hGFShIsBTwwsRoLe22g_G_t_0Qg';
+
+const String rpcUrl = "http://127.0.0.1:7545";
+//"https://rpc-mumbai.maticvigil.com/v1/a5be973518c173bacd9be16a6314dd08b6abcd23";
+
+const String contractAddress = "0xdd0b003e0e89DdF9d95d765a1f5C5157238Fe1Ec";
+
+const int chainId = 1337; //Polygon testnet-80001, local ganache-1337
